43eb4b0ee95478743c992155a10a0a7390abe3de
rafaseron <rafaelseron3011@gmail.com>
2024-02-12 18:11:14 -0400

    ViewHolder class

--- app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
@@ -3,24 +3,40 @@ package br.com.igorbag.githubsearch.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.domain.Repository
+import org.w3c.dom.Text
 
-class RepositoryAdapter(private val repositories: List<Repository>) : RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
+class RepositoryAdapter(private val repositories: List<Repository>) : RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder>() {
 
     var repoItemLister: (Repository) -> Unit = {}
     var btnShareLister: (Repository) -> Unit = {}
 
+    class RepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val txtRepositoryName: TextView
+
+        init {
+            txtRepositoryName = itemView.findViewById(R.id.txtRepositoryName)
+        }
+
+
+
+    }
+
     // Cria uma nova view
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.repository_item, parent, false)
-        return ViewHolder(view)
+        return RepositoryViewHolder(view)
     }
 
     // Pega o conteudo da view e troca pela informacao de item de uma lista
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
+        holder.txtRepositoryName.text = repositories[position].name
+
         //@TODO 8 -  Realizar o bind do viewHolder
         //Exemplo de Bind
         //  holder.preco.text = repositories[position].atributo
